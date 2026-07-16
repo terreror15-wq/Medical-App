f2f91f11439f642dea1c5584bc76381a9304592d
terreror15-wq <terreror15@gmail.com>
2026-07-16 19:43:25 -0400

    Add 'Genero' property to Patient class
    Added a property 'Genero' to the Patient class.

--- Medical App/MedicalApp.Domain/Entities/Patient.cs
+++ Medical App/MedicalApp.Domain/Entities/Patient.cs
@@ -12,7 +12,7 @@ namespace MedicalApp.Domain.Entities
         public string LastName { get; set; } = string.Empty;
         public string IdentityDocument { get; set; } = string.Empty;
         public DateTime DateBirth { get; set; }
-        //public Genero Genero { get; set; }
+        public Genero Genero { get; set; }
         public string? Phone { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
