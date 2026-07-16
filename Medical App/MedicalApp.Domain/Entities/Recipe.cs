76d3e7c905d483ef40aa268f0ddf8aa8ab2d64d0
terreror15-wq <terreror15@gmail.com>
2026-07-16 18:37:49 -0400

    Add files via upload
    Adding the others classes

---
+++ Medical App/MedicalApp.Domain/Entities/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MedicalApp.Domain.Core;
+
+namespace MedicalApp.Domain.Entities
+{
+    public class Recipe: BaseEntity
+    {
+        public DateTime EmisionDate { get; set; } = DateTime.UtcNow;
+        public string? Obsevation { get; set; }
+
+        /*
+         Traducir al ingles 
+           public int PacienteId { get; set; }
+    public Paciente Paciente { get; set; } = null!;
+    public int DoctorId { get; set; }
+    public Doctor Doctor { get; set; } = null!;
+    public ICollection<RecetaDetalle> Detalles { get; set; } = new List<RecetaDetalle>();
+         */
+    }
+}
---
+++ Medical App/MedicalApp.Domain/Entities/RecipeDetail.cs
@@ -0,0 +1,21 @@
+﻿using MedicalApp.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalApp.Domain.Entities
+{
+    public class RecipeDetail:BaseEntity
+    {
+        public int Id { get; set; }
+        public string Dosis { get; set; } = string.Empty;
+        public string Frecuency { get; set; } = string.Empty;
+        public int DurationDays { get; set; }
+
+        /*
+         public int RecipeId {get;set;}
+        public Recipe Recipe {get;set;}= null!;
+        
+         */
+    }
+}
