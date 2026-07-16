7cae6721f8266b2cb8fa1a462fcb1eb21549a03e
terreror15-wq <terreror15@gmail.com>
2026-07-16 18:39:07 -0400

    Adding others Entities

---
+++ Medical App/MedicalApp.Domain/Entities/MedicalHistory.cs
@@ -0,0 +1,23 @@
+﻿using MedicalApp.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalApp.Domain.Entities
+{
+    public class MedicalHistory : BaseEntity
+    {
+      
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public string ReasonVisit { get; set; } = string.Empty;
+        public string? Symptoms { get; set; }
+        public string? Observations { get; set; }
+        //public int PacienteId { get; set; }
+        // public Paciente Paciente { get; set; } = null!;
+        // public int DoctorId { get; set; }
+        // public Doctor Doctor { get; set; } = null!;
+        //public int? CitaId { get; set; }
+        //public Cita? Cita { get; set; }
+        //public ICollection<Diagnostico> Diagnosticos { get; set; }
+    }
+}
