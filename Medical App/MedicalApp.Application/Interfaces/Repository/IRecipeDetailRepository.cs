12bdb8333b5110cdf06cf0b45ce48501cda512f8
terreror15-wq <terreror15@gmail.com>
2026-07-16 21:41:33 -0400

    Adding interfaces Repositories

---
+++ Medical App/MedicalApp.Application/Interfaces/Repository/IRecipeDetailRepository.cs
@@ -0,0 +1,18 @@
+﻿using MedicalApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalApp.Application.Interfaces
+{
+    public interface IRecipeDetailRepository
+    {
+        public Task<IEnumerable<RecipeDetail>> GetAllRecipeDetail();
+        public Task<RecipeDetail>GetRecipeDetailById(int id);
+        public Task<RecipeDetail>AddRecipeDetail(RecipeDetail recipedetail);
+        public Task<RecipeDetail>RemuveRecipeDetail(int id);
+        public Task<RecipeDetail> UpdateRecipeDetail(RecipeDetail recipeDetail, int id);
+    }
+}
