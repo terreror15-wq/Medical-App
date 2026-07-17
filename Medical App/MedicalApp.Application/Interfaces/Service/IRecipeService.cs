72bc13453772aeab6b75a3745d076299ca8946fa
terreror15-wq <terreror15@gmail.com>
2026-07-16 21:49:58 -0400

    Adding other services

---
+++ Medical App/MedicalApp.Application/Interfaces/Service/IRecipeService.cs
@@ -0,0 +1,18 @@
+﻿using MedicalApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalApp.Application.Service
+{
+    public interface IRecipeService
+    {
+        public Task<IEnumerable<Recipe>> GetAllRecipe();
+        public Task<Recipe> GetRecipeById(int id);
+        public Task<Recipe> AddRecipe(Recipe recipe);
+        public Task<Recipe> RemuveRecipe(int id);
+        public Task<Recipe> UpdateRecipe(int id, Recipe recipe);
+    }
+}
