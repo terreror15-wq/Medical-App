add796b96d1cc37215ad1e9fc77e675db5ea42dd
terreror15-wq <terreror15@gmail.com>
2026-07-16 21:55:20 -0400

    Adding IAppointmentService

---
+++ Medical App/MedicalApp.Application/Interfaces/Service/IAppointmentService.cs
@@ -0,0 +1,17 @@
+﻿using MedicalApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalApp.Domain.Interfaces.Services
+{
+    public interface IAppointmentService
+    {
+        public Task<Appointment> GetAppointmentById();
+        public Task<IEnumerable<Appointment>> GetAppointment();
+        public Task AddAppointment(Appointment appointment);
+        public Task RemoveAppointment(int id);
+        public Task UpdateAppointment(Appointment appointment, int id);
+
+    }
+}
