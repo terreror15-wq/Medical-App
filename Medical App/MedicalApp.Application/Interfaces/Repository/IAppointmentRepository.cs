56fe660a2efb114cd2d066f0f61afb944a94bfcd
terreror15-wq <terreror15@gmail.com>
2026-07-20 19:17:09 -0400

    Add files via upload

---
+++ Medical App/MedicalApp.Application/Interfaces/Repository/IAppointmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using MedicalApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalApp.Application.Interfaces.Repository
+{
+    public interface IAppointmentRepository
+    {
+        public Task<Appointment> GetAppointmentById (int id);
+        public Task<IEnumerable<Appointment>> GetAllAppointment();
+        public Task UpdateAppointment(int id, Appointment appointment);
+        public Task RemuveAppointment(int id);
+        public Task AddDAppointment(Appointment appointment);
+    }
+}
