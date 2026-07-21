9690ee0dcb8c26aea08754a20185c0a88eeb7eca
terreror15-wq <terreror15@gmail.com>
2026-07-21 04:39:41 -0400

    Update connection string to use a placeholder
    Replaced the database connection string with a placeholder for security.

--- Medical App/MedicalApp.Infraestructure/Data/MedicalAppDbContextFactory.cs
+++ Medical App/MedicalApp.Infraestructure/Data/MedicalAppDbContextFactory.cs
@@ -11,7 +11,7 @@ namespace MedicalApp.Infraestructure.Data
         public MedicalAppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<MedicalAppDbContext>();
-            optionsBuilder.UseNpgsql("Host=aws-0-ca-central-1.pooler.supabase.com;Database=postgres;Username=postgres.wjkgfclsvfuzhbzzyips;Password=Medical.com0011;SSL Mode=Require;Trust Server Certificate=true");
+            optionsBuilder.UseNpgsql("YOUR PASSWORD");
 
             return new MedicalAppDbContext(optionsBuilder.Options);
         }
