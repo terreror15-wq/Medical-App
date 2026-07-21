e70f40c58a87d6dde27f1b0abfe4944ba5df87c6
terreror15-wq <terreror15@gmail.com>
2026-07-21 18:39:17 -0400

    update program
    Configured database context and added CORS policy for frontend.

--- Medical App/MedicalApp.Api/Program.cs
+++ Medical App/MedicalApp.Api/Program.cs
@@ -1,41 +1,94 @@
+using MedicalApp.Application.Interfaces;
+using MedicalApp.Application.Interfaces.Repository;
+using MedicalApp.Application.Interfaces.Service;
+using MedicalApp.Application.Service;
+using MedicalApp.Application.Services;
+using MedicalApp.Domain.Entities;
+using MedicalApp.Domain.Interfaces.Services;
+using MedicalApp.Infraestructure.Data;
+using MedicalApp.Infraestructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var MyConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MedicalAppDbContext>(options =>
+{
+    options.UseNpgsql(MyConnectionString);
+});
+
+
+const string FrontendCorsPolicy = "FrontendCorsPolicy";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(FrontendCorsPolicy, policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
+
+builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+builder.Services.AddScoped<IDoctorService, DoctorService>();
+
+
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+
+builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
+
+
+builder.Services.AddScoped<IDiagnosisRepository, DiagnosisRepository>();
+builder.Services.AddScoped<IDiagnosisServcice, DiagnosisService>();
+
+
+builder.Services.AddScoped<IMedicalHistoryRepository, MedicalHistoryRepository>();
+builder.Services.AddScoped<IMedicalHistoryService, MedicalHistoryService>();
+
+
+builder.Services.AddScoped<IMedicalOfficeRepository, MedicalOfficeRepository>();
+builder.Services.AddScoped<IMedicalOfficeService, MedicalOfficeService>();
+
+
+builder.Services.AddScoped<IMedicineRepository, MedicineRepository>();
+builder.Services.AddScoped<IMedicineService, MedicineService>();
+
+
+builder.Services.AddScoped<IRecipeRepository, RecipeRepository>();
+builder.Services.AddScoped<IRecipeService, RecipeService>();
+
+
+builder.Services.AddScoped<IRecipeDetailRepository, RecibeDetailRepository>();
+builder.Services.AddScoped<IRecipeDetailService, RecipeDetailService>();
+
+
+builder.Services.AddScoped<ISpecialyRepository, SpecialyRepository>();
+builder.Services.AddScoped<ISpecialyService, SpecialyService>();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors(FrontendCorsPolicy);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapSwagger();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
