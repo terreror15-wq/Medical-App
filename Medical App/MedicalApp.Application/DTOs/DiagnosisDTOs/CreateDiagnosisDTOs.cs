45fc137143c2dd3ac70fab79525d2bdd435dce7f
terreror15-wq <terreror15@gmail.com>
2026-07-21 04:05:04 -0400

    Adding others DTOs

---
+++ Medical App/MedicalApp.Application/DTOs/DiagnosisDTOs/CreateDiagnosisDTOs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalApp.Application.DTOs.DiagnosisDTOs
+{
+    public class CreateDiagnosisDTOs
+    {
+        public string? CodeCIE { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Medical App/MedicalApp.Application/DTOs/DiagnosisDTOs/ReadDiagnosisDTOs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalApp.Application.DTOs.DiagnosisDTOs
+{
+    public class ReadDiagnosisDTOs
+    {
+        public string? CodeCIE { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+    
+    }
+}
